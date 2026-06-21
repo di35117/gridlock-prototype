dd106264fca89a50e768b2878e5cd7d31b6304e4
dishank choudhury <dishankchoudhury@gmail.com>
2026-06-21 09:26:04 +0530

    Refactor Vite configuration by removing MapmyIndia proxy settings and cleaning up code formatting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    // THIS IS THE BYPASS: Intercepts MapmyIndia requests and masks the Origin
-    proxy: {
-      "/mapmyindia": {
-        target: "https://apis.mapmyindia.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/mapmyindia/, ""),
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
